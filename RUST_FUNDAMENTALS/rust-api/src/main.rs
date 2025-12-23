714470aa592632a9f42250aec950ca3bedda4e58
Ali Hassan <alihassan26032004@gmail.com>
2025-12-23 21:11:47 +0500

    rust-api/v1/dependencies/v1

--- RUST_FUNDAMENTALS/rust-api/src/main.rs
+++ RUST_FUNDAMENTALS/rust-api/src/main.rs
@@ -1,3 +1,14 @@
+use axum:: Router;
+use dotenvy:: dotenv;
+use std::net::SocketAddr;
+
+
+#[tokio::main]
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let app = Router::new()
+    let addr  = SocketAddr::from(([127,0,0,1],3000));
+    println!("Server is running at: {}",addr)
+
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap()
 }
