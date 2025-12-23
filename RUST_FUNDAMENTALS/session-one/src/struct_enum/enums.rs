9c4c28829f9eabc1ba09cc95120d658d58ef7b96
Ali Hassan <alihassan26032004@gmail.com>
2025-12-23 19:45:46 +0500

    quarter.time state added in enums

--- RUST_FUNDAMENTALS/session-one/src/struct_enum/enums.rs
+++ RUST_FUNDAMENTALS/session-one/src/struct_enum/enums.rs
@@ -17,7 +17,7 @@ fn check_day() {
 
 #[derive(Debug)]
 enum Coin{
-    nickel,
+    Nickel,
     Penny,
     Dime,
     Quarter(u8),//  unsingned integer 8 bit
@@ -27,27 +27,27 @@ enum Message{
     Quit,
     Move {x: i32, y:i32},
     Write (String),
-    change_color(i32,i32,i32)
+    changeColor(i32,i32,i32)
 }
 
 pub fn run() {
     check_day();
     let c = Coin::Quarter(5);
     println!("Coin: {:?}",c);
-
+    let Message_Quit:Message = Message::Quit;
     let message_one:Message = Message::Write(String::from("Wow signal"));
     let message_two:Message = Message::Move { x: 20, y: 30 };
-    let message_to_change_color: Message = Message::change_color(128,127, 126);
-
+    let message_to_change_color: Message = Message::changeColor(128,127, 126);
+    println!("Write_Message: {:?}, Move_Message: {:?}, change Color: {:?}",message_one,message_two,message_to_change_color);
     let coin:Coin = Coin::Quarter(3);
-    println!("Value {}",value_in_cents(coin))
+    println!("Value {}",value_in_cents(coin));
 }
 
 
 fn value_in_cents(coin:Coin ) -> u8{
     match coin{
         Coin::Penny => 1,
-        Coin::nickel =>3,
+        Coin::Nickel =>3,
         Coin::Dime =>5,
         Coin::Quarter(state)=>{
             println!("The quarter value is: {}",state);
